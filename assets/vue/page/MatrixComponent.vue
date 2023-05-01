0d7230fa913f685f874874cf80986dda7cdf7d89
mark <totubalin.m@perfluence.io>
2023-05-01 11:56:59 +0400

    add vue js

---
+++ assets/vue/page/MatrixComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{ test }}!</div>
+</template>
+
+<script>
+  export default {
+    name: 'matrix',
+    data() {
+      return {
+        test: "test data",
+      }
+    },
+  };
+</script>
