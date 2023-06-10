ebe0204c4ea0173fb9533ab04965274bb0075c02
mark <totubalin.m@perfluence.io>
2023-06-10 18:52:57 +0400

    rename title

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -33,7 +33,7 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Методы принятия решений')
+            ->setTitle('Задачи принятия решений')
         ;
     }
 
